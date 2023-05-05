0e6cf0785c7f2d7e1ff45fe9dcf9081602748423
JavaHunk2020 <javahunk2020@gmail.com>
2023-05-06 02:33:02 +0530

    code base for signup and show all

---
+++ src/main/java/p1/pykube/SignupDaoService.java
@@ -0,0 +1,70 @@
+package p1.pykube;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class SignupDaoService {
+	
+	
+	public static void save(SignupDTO signupDTO) {
+		try {
+			//
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/batch100_db","root","mysql@1234");
+			String sql="insert into psignup_tbl(username,password,email,mobile,address,doe) values(?,?,?,?,?,?)";
+			PreparedStatement pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, signupDTO.getUsername());
+			pstmt.setString(2, signupDTO.getPassword());
+			pstmt.setString(3, signupDTO.getEmail());
+			pstmt.setLong(4, Long.parseLong(signupDTO.getMobile()));
+			pstmt.setString(5, signupDTO.getAddress());
+			Timestamp doe=new Timestamp(new Date().getTime());
+			pstmt.setTimestamp(6, doe);
+			//FIRE THE QUERY
+			pstmt.executeUpdate();
+		}catch (Exception e) {
+			e.printStackTrace();	
+		}
+	}
+	
+	
+	static List<SignupDTO> findAll(){
+		List<SignupDTO> signupDTOs=new ArrayList<SignupDTO>();
+		try {
+			//
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/batch100_db","root","mysql@1234");
+			String sql="select pid,username,password,email,mobile,address,doe from psignup_tbl";
+			PreparedStatement pstmt=conn.prepareStatement(sql);
+			//FIRE THE QUERY
+			ResultSet rs=pstmt.executeQuery();
+			while(rs.next()) {
+				//Creating object for each row
+				SignupDTO signupDTO=new SignupDTO();
+				signupDTO.setPid(rs.getInt(1));
+				signupDTO.setUsername(rs.getString(2));
+				signupDTO.setPassword(rs.getString(3));
+				signupDTO.setEmail(rs.getString(4));
+				//valueOf convets Long into String object
+				signupDTO.setMobile(String.valueOf(rs.getLong(5)));
+				signupDTO.setAddress(rs.getString(6));
+				signupDTO.setDoe(rs.getTimestamp(7));
+				//Every object we are adding inside list
+				signupDTOs.add(signupDTO);
+			}
+		}catch (Exception e) {
+			e.printStackTrace();	
+		}
+		
+		return signupDTOs;
+	}
+	
+	
+
+}
---
+++ src/main/webapp/showSignup.jsp
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<%@page import="p1.pykube.SignupDTO"%>
+<%@page import="java.util.List"%>
+<html>
+<head>
+ <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+   <header style="background-color: yellow;height: 30px;">
+   </header>
+
+ <div  class="container">
+ 	  <h2>Signup Page</h2>
+ 	  
+ 	  <img  src="students_schoool_footer2.png"  style="height: 180px;"/>
+        <img  src="students_schoool_footer2.png"  style="height: 180px;"/>
+          <img  src="students_schoool_footer2.png"  style="height: 180px;"/>
+         <hr/>
+        
+        <h1>PROFILE DATA!</h1>
+        <span style="color:red;font-size: 16px;font-weight: bold;">${message}</span>
+         <table class="table table-bordered">
+    <thead>
+      <tr>
+      <th>Pid</th>
+        <th>Username</th>
+        <th>Password</th>
+        <th>Email</th>
+         <th>Mobile</th>
+         <th>Address</th>
+         <th>Created Date</th>
+         <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+    
+      <%
+		 List<SignupDTO> dtos=(List<SignupDTO>)request.getAttribute("signups");
+        for(SignupDTO signupDTO : dtos){        
+      %>
+      <tr>
+         <td><%=signupDTO.getPid()%></td>
+        <td><%=signupDTO.getUsername()%></td>
+       <td><%=signupDTO.getPassword()%></td>
+        <td><%=signupDTO.getEmail()%></td>
+         <td><%=signupDTO.getMobile()%></td>
+          <td><%=signupDTO.getAddress()%></td>
+             <td><%=signupDTO.getDoe()%></td>
+             <td>
+              <a href="#">
+              <button type="button" class="btn btn-danger">DELETE</button>
+              </a>
+             </td>
+      </tr>
+      
+      <% } %>
+      
+    </tbody>
+  </table>
+        
+ </div>
+
+</body>
+</html>
